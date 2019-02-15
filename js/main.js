6f55a77e2b6707abd95baa4b36740c171a71d97d
Daniel Gawne <danielgawne@gmail.com>
2019-02-15 15:58:54 -0500

    create bookmarks JSON array if one doesn't exist and push new bookmark to array

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Listen for form Submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+// Save Bookmark
+function saveBookmark(e){
+  // Get form values
+  var siteName = document.getElementById('siteName').value;
+  var siteUrl = document.getElementById('siteUrl').value;
+
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+  // Test if bookmark is null
+  if(localStorage.getItem('bookmarks') === null) {
+    // init array
+    var bookmarks = [];
+    // add to array
+    bookmarks.push(bookmark);
+    // set to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
+  /*
+  // Local storage test
+  localStorage.setItem('test', 'Hello World');
+  console.log(localStorage.getItem('test'));
+  localStorage.removeItem('test');
+  console.log(localStorage.getItem('test'));
+  */
+
+  // prevent form from submitting
+  e.preventDefault();
+}
